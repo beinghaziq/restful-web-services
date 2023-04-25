7ec5b789c9224f6e08caa685e6e5af8da066182a
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-04-25 17:13:34 +0500

    More on api versioning (#12)
    * request param versioning added
    
    * header type versioning added
    
    * media type versioning added

--- src/main/java/com/haziqjava/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/haziqjava/restfulwebservices/versioning/VersioningPersonController.java
@@ -15,4 +15,34 @@ public class VersioningPersonController {
   public PersonV2 getSecondVersionOfPerson() {
     return new PersonV2(new Name("Bob", "Charlie"));
   }
+
+  @GetMapping(path = "/person", params = "version=1")
+  public PersonV1 getFirstVersionOfPersonWithParams() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping(path = "/person", params = "version=2")
+  public PersonV2 getSecondVersionOfPersonWithParams() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
+
+  @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+  public PersonV1 getFirstVersionOfPersonWithHeaders() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+  public PersonV2 getSecondVersionOfPersonWithHeaders() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
+
+  @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+  public PersonV1 getFirstVersionOfPersonWithAcceptHeaders() {
+    return new PersonV1("Bob Charlie");
+  }
+
+  @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+  public PersonV2 getSecondVersionOfPersonWithAcceptHeaders() {
+    return new PersonV2(new Name("Bob", "Charlie"));
+  }
 }
