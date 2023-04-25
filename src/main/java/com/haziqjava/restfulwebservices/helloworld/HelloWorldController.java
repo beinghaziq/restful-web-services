24436e5c301a4d9dae8e4e23969d13cd97534085
Muhammad Haziq <beinghaziq@gmail.com>
2023-04-25 15:14:52 +0500

    internationalization added english and urdu

--- src/main/java/com/haziqjava/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/haziqjava/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.haziqjava.restfulwebservices.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+  private MessageSource messageSource;
+
+  public HelloWorldController(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World one";
@@ -19,5 +28,11 @@ public class HelloWorldController {
   @GetMapping(path = "/hello-world/path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
+  }
+
+  @GetMapping(path = "/hello-world-internationized")
+  public String helloWorldI18n() {
+    Locale locale = LocaleContextHolder.getLocale();
+    return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
   }
 }
