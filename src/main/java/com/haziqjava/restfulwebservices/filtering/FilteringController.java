79c198d218ec6f36c228c178aab04afb80aab5e5
Muhammad Haziq <beinghaziq@gmail.com>
2023-04-26 15:55:54 +0500

    serailizingBeans private method added

--- src/main/java/com/haziqjava/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/haziqjava/restfulwebservices/filtering/FilteringController.java
@@ -17,27 +17,26 @@ public class FilteringController {
   @GetMapping("/filtering")
   public MappingJacksonValue filtering() {
     SomeBean someBean = new SomeBean("value 1", "value 2", "value 3");
-//    INFO: A simple holder for the POJO to serialize via MappingJackson2HttpMessageConverter along
-//    with further serialization instructions to be passed in to the converter.
-//    On the server side this wrapper is added with a ResponseBodyInterceptor after content negotiation
-//    selects the converter to use but before the write.
-    MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
-    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field3");
-    FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
-    mappingJacksonValue.setFilters(filters);
-    return mappingJacksonValue;
+    return serializingBeans(someBean, "field1", "field3");
   }
 
   @GetMapping("/filtering-list")
   public MappingJacksonValue filteringList() {
     List<SomeBean> someBeans = Arrays.asList(new SomeBean("value 1", "value 2", "value 3"),
             new SomeBean("value 13", "value 6", "value 4"));
-    MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBeans);
-    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2", "field3");
-    FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
-    mappingJacksonValue.setFilters(filters);
-
-    return mappingJacksonValue;
+    return serializingBeans(someBeans, "field2", "field3");
   }
 
+//  Info: Serializing
+  private MappingJacksonValue serializingBeans(Object data, String field1, String field2) {
+    //    INFO: A simple holder for the POJO to serialize via MappingJackson2HttpMessageConverter along
+//    with further serialization instructions to be passed in to the converter.
+//    On the server side this wrapper is added with a ResponseBodyInterceptor after content negotiation
+//    selects the converter to use but before the write.
+    MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(data);
+    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept(field1, field2);
+    FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+    mappingJacksonValue.setFilters(filters);
+    return mappingJacksonValue;
+  }
 }
