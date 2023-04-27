31d0b4dba8886704c8f7782135d9f21b81100a2c
Muhammad Haziq <beinghaziq@gmail.com>
2023-04-27 18:44:48 +0500

    user class is connected to h2 DB

--- src/main/java/com/haziqjava/restfulwebservices/user/User.java
+++ src/main/java/com/haziqjava/restfulwebservices/user/User.java
@@ -1,12 +1,18 @@
 package com.haziqjava.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name="user_details")
 public class User {
+  @Id
+  @GeneratedValue
   private Integer id;
   @Size(min=2, message = "name must be greater than 2 characters")
   @JsonProperty("user_name") //INFO: will be sent as user_name in response
