a754bc7235685edb0939860179f17ba2654242cb
Muhammad Haziq <72576839+beinghaziq@users.noreply.github.com>
2023-05-01 18:48:58 +0500

    Post entity created (#20)
    * Post model created and data added
    
    * endline added

--- src/main/java/com/haziqjava/restfulwebservices/user/User.java
+++ src/main/java/com/haziqjava/restfulwebservices/user/User.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity(name="user_details")
 public class User {
@@ -22,6 +24,8 @@ public class User {
   @Past(message = "Birth date must be in past")
   @JsonProperty("birth_date")
   private LocalDate birthDate;
+  @OneToMany(mappedBy = "user") //User has many posts
+  private List<PostModel> posts;
 
   public User(Integer id, String name, LocalDate birthDate) {
     this.id = id;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(3, current_date(), 'xyz');
 
 insert into user_details(id,birth_date,name)
 values(4, current_date(), 'Legend');
+
+insert into post(id,description,user_id)
+values(1, 'My First', 1);
+
+insert into post(id,description,user_id)
+values(2, 'My Second', 1);
+
+insert into post(id,description,user_id)
+values(3, 'Legendary Post', 4);
+
+insert into post(id,description,user_id)
+values(4, 'Legendary Post One', 4);
